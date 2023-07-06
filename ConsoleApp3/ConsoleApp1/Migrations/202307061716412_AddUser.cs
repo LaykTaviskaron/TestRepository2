6e495d40e957e21623a104a3bd327a9810046f97
LaykTaviskaron <layk.taviskaron@gmail.com>
2023-07-06 21:06:56 +0300

    Entity framework example in console

---
+++ ConsoleApp3/ConsoleApp1/Migrations/202307061716412_AddUser.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUser : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        Phone = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Orders", "UserId");
+            AddForeignKey("dbo.Orders", "UserId", "dbo.Users", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropIndex("dbo.Orders", new[] { "UserId" });
+            DropTable("dbo.Users");
+        }
+    }
+}
